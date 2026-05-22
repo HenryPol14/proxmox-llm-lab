2ad7915eee2e0a0a6afa2a8b14f76d9e16251292
HenryPol14 <henrynik@gmail.com>
2026-05-22 20:45:44 +0300

    Добавлены комментарии в run-all.sh

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -2,9 +2,16 @@
 
 set -e
 
+# Подготовка Proxmox хоста.
 ./01-install-proxmox-tools.sh
 ./02-enable-iommu.sh
+
+# Загрузка и подготовка базового Ubuntu образа.
 ./03-download-cloud-image.sh
 ./04-create-cloudinit-template.sh
+
+# Создание и настройка LLM VM.
 ./05-create-llm-vm.sh
+
+# Создание отдельной VM для мониторинга.
 ./06-create-monitoring-vm.sh
